a2283f2834163dda35a81fdbe6016421c5fa5cf1
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-03 14:48:49 +0100

    Update page.tsx

--- app/app/dealers/[id]/page.tsx
+++ app/app/dealers/[id]/page.tsx
@@ -1,8 +1,8 @@
+// app/app/dealers/[id]/page.tsx
 export const dynamic = "force-dynamic";
 
 import DealerClient from "./DealerClient";
 
 export default function DealerPage({ params }: { params: { id: string } }) {
-  // Übergabe als Prop (zusätzlich zu useParams im Client) => doppelt robust
-  return <DealerClient id={params.id} />;
+  return <DealerClient dealerId={params.id} />;
 }
