3db0f793a7be780f089f22d7003b544ba133a2f0
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-04 00:39:25 +0100

    Delete app/app directory

--- app/app/dealers/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic";
-
-import DealerClient from "./DealerClient";
-
-export default function DealerPage({ params }: { params: { id: string } }) {
-  return <DealerClient dealerId={params.id} />;
-}
