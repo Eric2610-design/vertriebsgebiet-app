a3461d4c6cd1e199e9f69a2f31c6548568364011
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-04 01:32:24 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,15 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+type Dealer = {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+};
+
 export default function LeafletMap() {
+  const [dealers, setDealers] = useState<Dealer[]>([]);
+
+  useEffect(() => {
+    fetch("/api/dealers")
+      .then((r) => r.json())
+      .then(setDealers)
+      .catch(console.error);
+  }, []);
+
   return (
-    <MapContainer center={[50.1109, 8.6821]} zoom={6} style={{ height: "100%", width: "100%" }}>
-      <TileLayer attribution="© OpenStreetMap" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[50.1109, 8.6821]}>
-        <Popup>Test-Händler</Popup>
-      </Marker>
+    <MapContainer center={[50.11, 8.68]} zoom={6} style={{ height: "100%", width: "100%" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {dealers.map((d) => (
+        <Marker key={d.id} position={[d.lat, d.lng]}>
+          <Popup>{d.name}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
