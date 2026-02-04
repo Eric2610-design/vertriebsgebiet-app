a2db892db4f043e95146960d7e86061c72bf7098
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-04 02:39:25 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { dealers } from "../lib/dealers";
+import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import Link from "next/link";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import UploadBox, { UploadedDealer } from "./UploadBox";
 
-// Fix für Marker-Icons
+// Marker-Fix
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
@@ -17,34 +18,37 @@ L.Icon.Default.mergeOptions({
     "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
-
-
 export default function LeafletMap() {
-  return (
-    <MapContainer
-      center={[50.11, 8.68]}
-      zoom={6}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        attribution="© OpenStreetMap"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+  const [dealers, setDealers] = useState<UploadedDealer[]>([]);
 
-      {dealers.map((d) => (
-        <Marker key={d.id} position={[d.lat, d.lng]}>
-          <Popup>
-            <strong>{d.name}</strong>
-            <br />
-            {d.city}
-            <br />
-            <Link href={`/dealers/${d.id}`}>
-              Details öffnen →
-            </Link>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+  return (
+    <>
+      <UploadBox onUpload={setDealers} />
+
+      <MapContainer
+        center={[50.11, 8.68]}
+        zoom={6}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution="© OpenStreetMap"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {dealers.map((d) => (
+          <Marker key={d.id} position={[d.lat, d.lng]}>
+            <Popup>
+              <strong>{d.name}</strong>
+              <br />
+              {d.city}
+              <br />
+              <Link href={`/dealers/${d.id}`}>
+                Details öffnen →
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </>
   );
 }
-
