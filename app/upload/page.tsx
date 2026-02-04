f37830a8f71aa6cda9a67ce81bc4e808f20b7b83
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-04 03:01:38 +0100

    Add files via upload

---
+++ app/upload/page.tsx
@@ -0,0 +1,10 @@
+import UploadBox from "@/components/UploadBox";
+
+export default function UploadPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Dateien hochladen</h1>
+      <UploadBox />
+    </div>
+  );
+}
