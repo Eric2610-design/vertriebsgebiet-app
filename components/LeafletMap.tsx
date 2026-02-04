aa0db1152214488b719371170faac9eedd79cacd
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-04 02:25:38 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,7 +1,19 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix für Marker-Icons (wichtig für Next.js)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 export default function LeafletMap() {
   return (
@@ -10,10 +22,4 @@ export default function LeafletMap() {
       zoom={6}
       style={{ height: "100%", width: "100%" }}
     >
-      <TileLayer
-        attribution="© OpenStreetMap"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
-}
+      <Til
