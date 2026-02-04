9baa5e70fe5dc248253f63ed397e17e7495485a2
Eric2610-design <e.fuhrmann@flyer.ch>
2026-02-04 12:22:04 +0100

    Delete app directory

--- app/upload/page.tsx
+++
@@ -1,9 +0,0 @@
-import UploadBox from "../../components/UploadBox";
-
-export default function UploadPage() {
-  return (
-    <main style={{ padding: 40 }}>
-      <UploadBox />
-    </main>
-  );
-}
