468ee94ead097b939319d8f18ab9a79fb7c2c7d5
eric2610 <e.fuhrmann@flyer.ch>
2026-02-07 00:51:31 +0100

    .

--- app/ad/[email]/page.tsx
+++
@@ -1,6 +0,0 @@
-import RepClient from "./RepClient";
-
-export default async function RepPage({ params }: { params: Promise<{ email: string }> }) {
-  const p = await params;
-  return <RepClient email={decodeURIComponent(p.email)} />;
-}
