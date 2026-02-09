71f4e83d9cb3ec1710c4f585157ce3a36dfee29f
eric2610 <e.fuhrmann@flyer.ch>
2026-02-09 18:56:21 +0100

    .

--- app/dealer/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import DealerClient from "./DealerClient";
-
-export default async function DealerPage(
-  { params }: { params: Promise<{ id: string }> }
-) {
-  const { id } = await params;
-  return <DealerClient id={id} />;
-}
--- supabase/migrations/007_backorders_add_aa_date.sql
+++
@@ -1,3 +0,0 @@
--- Add parsed AA date column for backorders
-alter table public.backorder_items
-  add column if not exists col_aa_date date;
